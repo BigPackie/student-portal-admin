c171d021868446a58b9e9706af720c7d9fb12e21
Patrik Pindes <ppindes@gmail.com>
2019-12-04 16:04:18 +0700

    news detail form wip

--- src/app/news-edit/news-edit.page.ts
+++ src/app/news-edit/news-edit.page.ts
@@ -8,6 +8,7 @@ import { DataService } from '../services/data.service';
 import { NgForm } from '@angular/forms';
 import { NewsItem } from '../models/news.item';
 import { NewsItemDetail } from '../models/news.item.detail';
+import { HttpEventType } from '@angular/common/http';
 
 @Component({
   selector: 'app-news-edit',
@@ -170,5 +171,151 @@ export class NewsEditPage implements OnInit {
     await alert.present();
   }
 
+
+    //TODO: manual file uploading, put it into own component
+    fileData: File = null;
+    previewUrl:any = null;
+    fileUploadProgress: string = null;
+    uploadedFilePath: string = null;
+    imageReady : boolean = false;
+  
+    newsItemSavedId: string;
+  
+   
+    fileProgress(fileInput: any) {
+          this.fileData = <File>fileInput.target.files[0];
+          this.preview();
+    }
+   
+    preview() {
+        // Show preview 
+        var mimeType = this.fileData.type;
+        if (mimeType.match(/image\/*/) == null) {
+          return;
+        }
+    
+        var reader = new FileReader();      
+        reader.readAsDataURL(this.fileData); 
+        reader.onload = (_event) => { 
+          this.previewUrl = reader.result; 
+          this.imageReady = true;
+        }
+    }
+    
+    //TODO temporar solution, should be refactored
+    onSubmit() {
+        const formData = new FormData();
+          formData.append('file', this.fileData);
+          // this.dataService.post('url/to/your/api', formData)
+          //   .subscribe(res => {
+          //     console.log(res);
+          //     this.uploadedFilePath = res.data.filePath;
+          //     alert('SUCCESS !!');
+          //   })
+  
+          this.fileUploadProgress = '0%';
+  
+          let newsItem = {
+            name:"test news",
+            validFrom: new Date(),
+            validTo: new Date("1.1.2666"),
+            overviewImageBase64: this.previewUrl,   //added when picture is loaded
+           // testArray: new Array(20000000)  //to simulate high payload, so that progress is visible
+          };
+  
+          //TODO: handle creation failure
+          console.log(`Creating newsItem ${newsItem}`);
+          this.dataService.createNewsItemManually(newsItem).subscribe((res) => {
+            if(res.type === HttpEventType.UploadProgress) {
+              this.fileUploadProgress = Math.round(res.loaded / res.total * 100) + '%';
+              console.log(this.fileUploadProgress);
+            } else if (res.type === HttpEventType.Response) {
+              this.fileUploadProgress = '';
+              this.newsItemSavedId = res.body._id;
+              //this.loadNews();
+              console.log(res);      
+              alert('News succesfully saved!');
+            }
+          
+          });
+  
+          this.imageReady = false;
+    }
+  
+    //end of manual file uploading
+  
+     //TODO: manual file uploading, put it into own component
+     detailFileData: File = null;
+     detailPreviewUrl:any = null;
+     detailFileUploadProgress: string = null;
+     uploadedDetailFilePath: string = null;
+     detailImageReady : boolean = false;
+  
+     selectedNewsId: string;
+   
+     newsDetailItemSavedId: string;
+   
+    
+     detailProgress(fileInput: any) {
+           this.detailFileData = <File>fileInput.target.files[0];
+           this.detailPreview();
+     }
+    
+     detailPreview() {
+         // Show preview 
+         var mimeType = this.detailFileData.type;
+         if (mimeType.match(/image\/*/) == null) {
+           return;
+         }
+     
+         var reader = new FileReader();      
+         reader.readAsDataURL(this.detailFileData); 
+         reader.onload = (_event) => { 
+           this.detailPreviewUrl = reader.result; 
+           this.detailImageReady = true;
+         }
+     }
+     
+     //TODO temporar solution, should be refactored
+     onSubmitDetail() {
+         const formData = new FormData();
+           formData.append('file', this.detailFileData);
+           // this.dataService.post('url/to/your/api', formData)
+           //   .subscribe(res => {
+           //     console.log(res);
+           //     this.uploadedFilePath = res.data.filePath;
+           //     alert('SUCCESS !!');
+           //   })
+   
+           this.detailFileUploadProgress = '0%';
+   
+           let newsItemDetail = {
+             _id: this.selectedNewsId,
+             description:"some optional details description",  
+             imageBase64: this.detailPreviewUrl,   //added when picture is loaded
+            // testArray: new Array(20000000)  //to simulate high payload, so that progress is visible
+           };
+   
+           //TODO: handle creation failure
+           console.log(`Creating newsItemDetail ${newsItemDetail}`);
+           this.dataService.createNewsItemDetails(newsItemDetail).subscribe((res) => {
+             if(res.type === HttpEventType.UploadProgress) {
+               this.detailFileUploadProgress = Math.round(res.loaded / res.total * 100) + '%';
+               this.newsDetailItemSavedId = '';
+               console.log(this.detailFileUploadProgress);
+             } else if (res.type === HttpEventType.Response) {
+               this.detailFileUploadProgress = '';
+               this.newsDetailItemSavedId = res.body._id;
+               console.log(res);      
+               alert('NewsDetail succesfully saved!');
+             }
+           
+           });
+   
+           this.detailImageReady = false;
+     }
+   
+     //end of manual file uploading
+   
 
 }
